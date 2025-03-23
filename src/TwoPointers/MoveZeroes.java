a697ab59099966a4992fdf385410c664325c976a
Vasundhara-Tiwari <developer.vasundharatiwari@gmail.com>
2025-03-24 00:45:16 +0530

    MoveZeroes

---
+++ src/TwoPointers/MoveZeroes.java
@@ -0,0 +1,18 @@
+package TwoPointers;
+
+public class MoveZeroes {
+    public void moveZeroes(int[] nums) {
+        int i = 0, j = 0;
+        while(i < nums.length){
+            if(nums[i] != 0){
+                nums[j] = nums[i];
+                j++;
+            }
+            i++;
+        }
+        while(j < nums.length){
+            nums[j] = 0;
+            j++;
+        }
+    }
+}
