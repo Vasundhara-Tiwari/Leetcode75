110cc7813d502d7f8052b28d70fb3783ade2a72f
Vasundhara-Tiwari <developer.vasundharatiwari@gmail.com>
2025-03-24 00:50:26 +0530

    FindThePivot

---
+++ src/PrefixSum/FindThePivot.java
@@ -0,0 +1,18 @@
+package PrefixSum;
+
+public class FindThePivot {
+    public int pivotIndex(int[] nums) {
+        int totalSum = 0;
+        int leftSum = 0;
+        for (int num : nums) {
+            totalSum += num;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (leftSum == totalSum - leftSum - nums[i]) {
+                return i;
+            }
+            leftSum += nums[i];
+        }
+        return -1;
+    }
+}
